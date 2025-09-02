711f4898a076d95e1def76e3b2a7a8af27a403fe
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2025-09-02 18:58:53 +0530

    Time: 1 ms (98.01%) | Memory: 41.1 MB (52.68%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long left = 0, right = n;
+        while(left <= right){
+            long mid = left + (right - left) / 2;
+            long c = mid * (mid + 1) / 2;
+            if(c == n){
+                return (int)mid;
+            }
+            if(c < n){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return (int)right;
+    }
+}
